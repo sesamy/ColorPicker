cd15bda0c0ca5cf18b9016668228a3ded169a3f9
sesamy <amy.hori97@gmail.com>
2023-11-07 13:29:34 -0800

    tabs 1-3, minimal notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+
 // Write your Color component here
 
+//accepts props from a parent compound and renders the view
+const Color = (props) => {
+  return <div className={props.color}></div>;
+};
+
 const App = () => {
+  //invoke the useState, and deconstruct the selected Color and setSelectedColor from returned array
+  //recall that convention is [name, setName]
+  const [selectorColor, setSelectedColor] = useState("");
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
         <div className="red">red</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        {/* Three instances of the Color component */}
+        {/* passes the name of color class through the component, then uses that class to determine the corresponding color */}
+        <Color color="red" />
+        <Color color="blue" />
+        <Color color="green" />
+      </div>
     </div>
   );
 };
